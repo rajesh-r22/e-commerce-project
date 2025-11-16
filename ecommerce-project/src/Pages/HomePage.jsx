1888ab0ff866d8ca0e97438d797f3c8fe3587b8c
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2025-11-17 02:32:46 +0530

    Use axios to fetch the data

--- ecommerce-project/src/Pages/HomePage.jsx
+++ ecommerce-project/src/Pages/HomePage.jsx
@@ -1,17 +1,14 @@
+import axios from 'axios'
 import { Headers } from '../Components/Headers'
 import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
-    fetch('http://localhost:3000/api/products')
+    axios.get('http://localhost:3000/api/products')
     .then((response)=>{
-        return response.json();
-    }).then((data)=>{
-        console.log(data);
-    }).catch((error)=>{
-        console.log("error: "+error);
-    })
-
+        console.log(response.data);
+        
+    }) 
     return (
         <>
         
