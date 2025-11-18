c384019e559deaa0d37637b1475df44527c7a2b2
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2025-11-18 16:14:05 +0530

    Lift cart up to the app.jsx for use for child pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { Routes, Route } from 'react-router';
+import {useState , useEffect} from 'react';
 import { HomePage } from './Pages/HomePage';
 import { CheckoutPage} from './Pages/checkout/CheckoutPage'
 import { OrdersPage} from './Pages/OrdersPage';
@@ -6,11 +8,18 @@ import { TrackingPage } from './Pages/TrackingPage';
 import './App.css'
 
 function App() {
+  const [cart,setCart]=useState([])
+
+  useEffect(()=>{
+    axios.get('/api/cart-items')
+            .then((response)=>{
+                setCart(response.data);
+            })
+  },[])
   return (
-    
     <Routes>
-      <Route index element={<HomePage />}/>
-      <Route path="checkout" element={<CheckoutPage/>}/>
+      <Route index element={<HomePage cart={cart} />}/>
+      <Route path="checkout" element={<CheckoutPage cart={cart}/>}/>
       <Route path="orders" element={<OrdersPage/>}/>
       <Route path="tracking" element={<TrackingPage/>}/>
     </Routes>
