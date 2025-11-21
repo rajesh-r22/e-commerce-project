ef70853a5520e4b08d5da05635cb913c416b8b6b
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2025-11-22 03:34:44 +0530

    Create add to cart button in orders page

--- ecommerce-project/src/Pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/Pages/orders/OrdersPage.jsx
@@ -6,7 +6,7 @@ import { Headers } from '../../Components/Headers';
 import './OrdersPage.css';
 
 
-export function OrdersPage({ cart }) {
+export function OrdersPage({ cart ,loadCart }) {
     const [orders, setOrders] = useState([]);
     useEffect(() => {
         const fetchOrdersData= async ()=>{
@@ -52,6 +52,13 @@ export function OrdersPage({ cart }) {
 
                                 <div className="order-details-grid">
                                     {order.products.map((orderProduct) => {
+                                        const addToCart=async()=>{
+                                            await axios.post('/api/cart-items',{
+                                                productId:orderProduct.product.id,
+                                                quantity:1
+                                            });
+                                            await loadCart();
+                                        }
                                         return (
                                             <Fragment key={orderProduct.product.id}>
                                                 <div className="product-image-container">
@@ -68,7 +75,8 @@ export function OrdersPage({ cart }) {
                                                     <div className="product-quantity">
                                                         Quantity: {orderProduct.quantity}
                                                     </div>
-                                                    <button className="buy-again-button button-primary">
+                                                    <button className="buy-again-button button-primary"
+                                                    onClick={addToCart}>
                                                         <img className="buy-again-icon" src="images/icons/buy-again.png" />
                                                         <span className="buy-again-message">Add to Cart</span>
                                                     </button>
