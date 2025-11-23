7ad16ec5315cfee7f1d1c0ecba3c7e1db89912df
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2025-11-23 18:11:53 +0530

    Test clicking add to cart button

--- ecommerce-project/src/Pages/home/Product.jsx
+++ ecommerce-project/src/Pages/home/Product.jsx
@@ -68,6 +68,7 @@ export function Product({ product, loadCart }) {
             </div>
 
             <button className="add-to-cart-button button-primary"
+                data-testid="add-to-cart-button"
                 onClick={addTOCart}>
                 Add to Cart
             </button>
--- ecommerce-project/src/Pages/home/Product.test.jsx
+++ ecommerce-project/src/Pages/home/Product.test.jsx
@@ -1,6 +1,10 @@
 import { expect, it, describe, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import { Product } from './Product';
+
+vi.mock('axios');
 
 const product = {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -40,6 +44,26 @@ describe('Product Component', () => {
         expect(
             screen.getByText('87')
         ).toBeInTheDocument();
+    });
+
+    it('adds a product to add to cart ',async()=>{
+          const loadCart = vi.fn();
+
+        render(<Product product={product} loadCart={loadCart} />);
+
+        const user = userEvent.setup();
+        const addToCartButton = screen.getByTestId('add-to-cart-button')
+        await user.click(addToCartButton);
+
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+               productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+               quantity: 1 
+            }
+        )
+
+        expect(loadCart).toHaveBeenCalled();
     });
 
 })
