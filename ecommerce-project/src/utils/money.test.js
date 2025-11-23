1389e57f48c251b0870a4ca27d423b8a709267f5
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2025-11-23 17:07:35 +0530

    Create unit test for formatMoney

---
+++ ecommerce-project/src/utils/money.test.js
@@ -0,0 +1,15 @@
+import { expect, it, describe } from 'vitest';
+import { formatMoney } from './money';
+
+describe('formatMoney', () => {
+    it('formats 1999 cents as $19.99', () => {
+        expect(formatMoney(1999)).toBe('$19.99');
+    });
+
+    it('displays 2 decimals', () => {
+        expect(formatMoney(1000)).toBe('$10.00');
+        expect(formatMoney(1008)).toBe('$10.08')
+    });
+})
+
+
